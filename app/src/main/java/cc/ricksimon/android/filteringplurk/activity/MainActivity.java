6f2d5949f919d7132e6fd9f5f366fa46239c5c72
Simon <simon.hsu@fuhu.com>
2017-10-26 17:40:56 +0800

    move MainActivity to .actvity add PlurkConfig for storage APP key and APP secret

--- app/src/main/java/cc/ricksimon/android/filteringplurk/activity/MainActivity.java
+++ app/src/main/java/cc/ricksimon/android/filteringplurk/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package cc.ricksimon.android.filteringplurk;
+package cc.ricksimon.android.filteringplurk.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cc.ricksimon.android.filteringplurk.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/cc/ricksimon/android/filteringplurk/data/PlurkConfig.java
@@ -0,0 +1,10 @@
+package cc.ricksimon.android.filteringplurk.data;
+
+/**
+ * Created by Simon on 2017/10/26.
+ */
+
+public class PlurkConfig {
+    public static final String APIKey = "";
+    public static final String APISecret = "";
+}
